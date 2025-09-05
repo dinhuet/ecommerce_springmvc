409f425b31822ce1ee984c2dac407be2e649adf8
Dinh Tran <23020043@vnu.edu.vn>
2025-09-05 23:48:53 +0700

    product detail

--- src/main/java/com/dinh/todo/controller/client/ItemController.java
+++ src/main/java/com/dinh/todo/controller/client/ItemController.java
@@ -1,5 +1,7 @@
 package com.dinh.todo.controller.client;
 
+import com.dinh.todo.models.Product;
+import com.dinh.todo.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ItemController {
 
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
-    public String getProductPage(@PathVariable String id, Model model) {
+    public String getProductPage(@PathVariable Long id, Model model) {
+        Product product = productService.findById(id);
+        model.addAttribute("product", product);
         return "client/product/detail";
     }
 }
