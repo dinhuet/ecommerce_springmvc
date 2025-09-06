b6a4bee97c2428852b05139130b817acbf45ebff
Dinh Tran <23020043@vnu.edu.vn>
2025-09-06 16:31:12 +0700

    register view and registerDTO

--- src/main/java/com/dinh/todo/controller/client/HomePageController.java
+++ src/main/java/com/dinh/todo/controller/client/HomePageController.java
@@ -1,10 +1,13 @@
 package com.dinh.todo.controller.client;
 
 import com.dinh.todo.models.Product;
+import com.dinh.todo.models.dto.RegisterDto;
 import com.dinh.todo.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -22,5 +25,16 @@ public class HomePageController {
         List<Product> products = productService.findAll();
         model.addAttribute("products", products);
         return "client/homepage/show";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDto());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser")  RegisterDto registerUser) {
+        return "client/auth/register";
     }
 }
